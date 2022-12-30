d0d97529b24917f9fb269bb32799f0f8abcf2aaf
RafaCintra <rafael0cintra@gmail.com>
2022-12-30 03:40:13 -0300

    first commit

---
+++ src/pages/priorizacao/reversa/reversa.js
@@ -0,0 +1,8 @@
+function consulta() {
+  let elemnt = window.document.getElementById("consulta").value;
+  console.log(elemnt);
+  if(elemnt==="93"){
+    window.document.getElementById("email").innerText="dqs093@magazineluiza.com.br"
+  }
+}
+
